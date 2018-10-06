6af8ba8d66b796be74fa0fb35787b23523e33ce9
Armine Iradian <armirad@yahoo.ca>
2018-10-05 22:35:02 -0400

    final final

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name="httpc",
     version='0.1',
-    py_modules=['httpc2'],
+    py_modules=['httpc'],
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
-        httpc=httpc2:cli
+        httpc=httpc:cli
     ''',
 )
